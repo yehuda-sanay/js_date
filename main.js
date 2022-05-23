99126532431c78dd377d4040a9771628bcdce8dc
yehuda-sanay <yehudasanay1@gmail.com>
2022-05-23 11:22:56 +0300

    23.05.22

---
+++ main.js
@@ -0,0 +1,12 @@
+// function printDateAntTime(){
+//     var datetime= new Date();
+//     console.log(datetime);
+// }
+// printDateAntTime()
+
+function getDateString(date1){
+    var date1=""
+    var date2= Date(date1);
+    console.log(date2);
+}
+getDateString("06 23 1991")
